7f91e32002c9d54b08deb0dfb2048c40c0bb7f12
nydcunit <48498428+nydcunit@users.noreply.github.com>
2025-05-11 18:14:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:helloworld@cluster0.kapy7mg.mongodb.net/wolo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
 
 app.get('/', (req, res) => {
     res.send('Wolo Backend is running');
